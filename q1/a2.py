be8af9357dc323fd1ac6b55a1fc572a1bd24dda1
neil_dong <neil_dong@hengyuan.com.tw>
2024-12-20 12:07:06 +0800

    update md

--- q1/a2.py
+++ q1/a2.py
@@ -1,6 +1,8 @@
+#讀取file
 with open('words.txt', 'r') as file:
     lines = file.readlines()
 count = 0
+#將所有字母轉成小寫後進行配對
 for line in lines:
     count= count + line.lower().count('twinkle')    
 print(f'{count} twinkle')
